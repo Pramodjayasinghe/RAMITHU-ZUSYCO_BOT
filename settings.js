a959340861a9d08fb7b102ccd9d3fbda470b7051
Pramodjayasinghe <Pramodjayasinghe428@gmail.com>
2024-11-23 10:00:45 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,37 @@
-/*
+QUEENELISA;;;==wZxgUSH5EWOZzXpVEa2IHe2JFdsp0RWhTUwZGarNUWLBzSyhnQwd2bvl2bjIEeJVEVx0Eepwd=13343416749/*
+name: Node.js CI
 
+on:
+  push:
+    branches:
+      - main
+  pull_request:
+    branches:
+      - main
+
+jobs:
+  build:
+
+    runs-on: ubuntu-latest
+
+    strategy:
+      matrix:
+        node-version: [20.x]
+
+    steps:
+    - name: Checkout repository
+      uses: actions/checkout@v3
+
+    - name: Set up Node.js
+      uses: actions/setup-node@v3
+      with:
+        node-version: ${{ matrix.node-version }}
+
+    - name: Install dependencies
+      run: npm install
+
+    - name: Start application
+      run: npm start
 
 ███████╗██╗░░░██╗░██████╗██╗░░░██╗░█████╗░░█████╗░  ░░░░░░  ███╗░░░███╗██████╗░
 ╚════██║██║░░░██║██╔════╝╚██╗░██╔╝██╔══██╗██╔══██╗  ░░░░░░  ████╗░████║██╔══██╗
